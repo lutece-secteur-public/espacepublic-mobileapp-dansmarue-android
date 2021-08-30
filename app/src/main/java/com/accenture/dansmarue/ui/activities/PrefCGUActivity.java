188d19b8d0ff4de375cb3b971dda55f0b4e8ff1f
Corbel <vincent.corbel@accenture.com>
2021-08-30 16:15:54 +0200

    Ajout des CGU Plaine Commune

--- app/src/main/java/com/accenture/dansmarue/ui/activities/PrefCGUActivity.java
+++ app/src/main/java/com/accenture/dansmarue/ui/activities/PrefCGUActivity.java
@@ -48,17 +48,6 @@ public class PrefCGUActivity extends BaseActivity {
         WebView textViewCguPartFour = (WebView)  findViewById(R.id.cgu_four);
         textViewCguPartFour.loadDataWithBaseURL(null, getString(R.string.cgu_part_four), "text/html", "utf-8", null);
 
-        WebView textViewCguPartFive = (WebView)  findViewById(R.id.cgu_five);
-        textViewCguPartFive.loadDataWithBaseURL(null, getString(R.string.cgu_part_five), "text/html", "utf-8", null);
-
-        WebView textViewCguPartSix = (WebView)  findViewById(R.id.cgu_six);
-        textViewCguPartSix.loadDataWithBaseURL(null, getString(R.string.cgu_part_six), "text/html", "utf-8", null);
-
-        WebView textViewCguPartSeven = (WebView)  findViewById(R.id.cgu_seven);
-        textViewCguPartSeven.loadDataWithBaseURL(null, getString(R.string.cgu_part_seven), "text/html", "utf-8", null);
-
-        WebView textViewCguPartHeight = (WebView)  findViewById(R.id.cgu_height);
-        textViewCguPartHeight.loadDataWithBaseURL(null, getString(R.string.cgu_part_height), "text/html", "utf-8", null);
     }
 
     @Override
